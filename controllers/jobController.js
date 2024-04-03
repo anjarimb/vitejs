51f3768336d357d08e4827a93e075f5195a18ea1
anjarimb <anjarimb@gmail.com>
2024-04-03 11:30:43 +0700

    first commit

---
+++ controllers/jobController.js
@@ -0,0 +1,46 @@
+import Job from '../models/JobModel.js';
+import { StatusCodes } from 'http-status-codes';
+import { NotFoundError } from '../errors/customErrors.js';
+
+
+export const getAllJobs = async (req,res)=>{
+    const jobs = await Job.find({});
+    res.status(StatusCodes.OK).json({ jobs });
+};
+
+export const createJob = async (req,res)=>{
+    const { company, position } = req.body;
+
+    const job = await Job.create({ company, position });
+    res.status(StatusCodes.CREATED).json({ job });
+};
+
+export const getJob = async (req,res) => {
+    const { id } = req.params;
+
+    const job = await Job.findById(id);
+
+    if (!job) throw new NotFoundError(`no job with id ${id}`);
+    res.status(StatusCodes.OK).json({ job });
+};
+
+export const updateJob = async (req,res)=>{
+    const { id } = req.params;
+
+    const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+        new: true,
+    });
+
+    if (!updatedJob) throw new NotFoundError(`no job with id ${id}`);
+
+    res.status(StatusCodes.OK).json({ msg: 'job modified',  job: updatedJob });
+};
+
+export const deleteJob = async (req,res)=>{
+    const { id } = req.params;
+    const removedJob = await Job.findByIdAndDelete(id);
+  
+    if (!removedJob) throw new NotFoundError(`no job with id ${id}`);
+    
+    res.status(StatusCodes.OK).json({ job: removedJob });
+};
---
+++ routes/jobRouter.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+const router =  Router();
+
+import { 
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob
+} from "../controllers/jobController.js";
+import { validateJobInput } from "../middleware/validationMiddleware.js";
+
+router.route('/').get(getAllJobs).post(validateJobInput, createJob);
+router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob);
+
+export default router;
